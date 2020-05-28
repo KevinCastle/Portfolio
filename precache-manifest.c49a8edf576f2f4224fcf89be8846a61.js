632de68a342741e74f41e4e8d64668a3b6fe1bd5
kevincastle <kevinacastles@gmail.com>
2020-05-28 14:33:16 -0400

    Updates

---
+++ precache-manifest.c49a8edf576f2f4224fcf89be8846a61.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "012e9b24404409c2675b70296a9f9893",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "7ac06edba586bb777b5e",
+    "url": "/Portfolio/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "538ca85f43eea2f02b80",
+    "url": "/Portfolio/static/css/main.b576660b.chunk.css"
+  },
+  {
+    "revision": "7ac06edba586bb777b5e",
+    "url": "/Portfolio/static/js/2.bef3b931.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.bef3b931.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "538ca85f43eea2f02b80",
+    "url": "/Portfolio/static/js/main.576c9c4a.chunk.js"
+  },
+  {
+    "revision": "bbf9f513ca94ccde67a9",
+    "url": "/Portfolio/static/js/runtime-main.88d4256f.js"
+  }
+]);
