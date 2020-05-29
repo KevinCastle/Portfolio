d774af18531a6d3e852248c2bfa457334c92cf68
kevincastle <kevinacastles@gmail.com>
2020-05-28 20:29:03 -0400

    Updates

--- precache-manifest.f0f28a67ab547d2e267be71b1d59588c.js
+++ precache-manifest.f0f28a67ab547d2e267be71b1d59588c.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c278216875df0a6ae5650fb308a3ed2b",
+    "revision": "6b83efa244d94c82011a0eb99567b09d",
     "url": "/Portfolio/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Portfolio/static/css/2.764ccc25.chunk.css"
   },
   {
-    "revision": "63c856cbbdaecfb03344",
-    "url": "/Portfolio/static/css/main.1c279538.chunk.css"
+    "revision": "8c46bf6a6a0c4521cd0c",
+    "url": "/Portfolio/static/css/main.47fdf87b.chunk.css"
   },
   {
     "revision": "7ac06edba586bb777b5e",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Portfolio/static/js/2.bef3b931.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "63c856cbbdaecfb03344",
-    "url": "/Portfolio/static/js/main.39f1d89b.chunk.js"
+    "revision": "8c46bf6a6a0c4521cd0c",
+    "url": "/Portfolio/static/js/main.578df86a.chunk.js"
   },
   {
     "revision": "bbf9f513ca94ccde67a9",
