d3f4a633af6cdcc893e3e608d27c22ad11e5ecd9
KevCastles <kevinacastles@gmail.com>
2020-08-31 17:35:24 -0400

    Add Landing page and Weather App

--- src/components/WorkPage.js
+++ src/components/WorkPage.js
@@ -5,48 +5,62 @@ const WorkPage = () => (
     <div id="work">
         <p className="work-title">Stuff I've Done<span className="work-dot">:</span></p>
         <div className="work-container">
-                <WorkItem
-                    itemDir={"https://infinite-tundra-21082.herokuapp.com"}
-                    images="ExpensifyBanner.png"
-                    name={"Expensify App"}
-                    framework={"React - SASS - Redux"}
-                    age={2020}
-                />
-                <WorkItem
+            <WorkItem
+                itemDir={"https://kevincastle.github.io/weather-app/"}
+                images="SimonBanner.png"
+                name={"Weather App"}
+                framework={"React - Sass - Hooks - OpenWeatherMap"}
+                age={2020}
+            />
+            <WorkItem
+                itemDir={"https://infinite-tundra-21082.herokuapp.com"}
+                images="ExpensifyBanner.png"
+                name={"Expensify App"}
+                framework={"React - Sass - Redux"}
+                age={2020}
+            />
+            <WorkItem
                 itemDir={"https://kevincastle.github.io/ChileTour/"}
-                    images="MychiletourBanner.png"
-                    name={"My Chile Tour"}
-                    framework={"React - React-Bootstrap"}
-                    age={2020}
-                />
-                <WorkItem
+                images="MychiletourBanner.png"
+                name={"My Chile Tour"}
+                framework={"React - React-Bootstrap"}
+                age={2020}
+            />
+            <WorkItem
+                itemDir={"https://kevincastle.github.io/example-landing-page/"}
+                images="MychiletourBanner.png"
+                name={"Example Landing Page"}
+                framework={"HTML - Bootstrap - JavaScript Vanilla"}
+                age={2020}
+            />
+            <WorkItem
                 itemDir={"https://fathomless-chamber-54195.herokuapp.com"}
-                    images="IndecisionBanner.png"
-                    name={"Indecision App"}
-                    framework={"React - SCSS - NodeJS"}
-                    age={2019}
-                />
-                <WorkItem
+                images="IndecisionBanner.png"
+                name={"Indecision App"}
+                framework={"React - SCSS - NodeJS"}
+                age={2019}
+            />
+            <WorkItem
                 itemDir={"https://kevincastle.github.io/Novotour/"}
-                    images="NovotourBanner.png"
-                    name={"Novotour Webpage"}
-                    framework={"HTML - CSS - PHP"}
-                    age={2018}
-                />
-                <WorkItem
+                images="NovotourBanner.png"
+                name={"Novotour Webpage"}
+                framework={"HTML - CSS - PHP"}
+                age={2018}
+            />
+            <WorkItem
                 itemDir={"https://codepen.io/KevinCastle/full/egmYYo"}
-                    images="TictactoeBanner.png"
-                    name={"Tic Tac Toe"}
-                    framework={"JQuery - CSS"}
-                    age={2016}
-                />
-                <WorkItem
+                images="TictactoeBanner.png"
+                name={"Tic Tac Toe"}
+                framework={"JQuery - CSS"}
+                age={2016}
+            />
+            <WorkItem
                 itemDir={"https://codepen.io/KevinCastle/full/LxVWme"}
-                    images="SimonBanner.png"
-                    name={"Simon Game"}
-                    framework={"JQuery - Bootstrap"}
-                    age={2016}
-                />               
+                images="SimonBanner.png"
+                name={"Simon Game"}
+                framework={"JQuery - Bootstrap"}
+                age={2016}
+            />
         </div>
     </div>
 );
